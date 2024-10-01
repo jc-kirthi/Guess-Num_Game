0a392117ab48a6f51e02bc8a4a009345d631534e
Kirthi JC <its.me.jckirthi@gmail.com>
2024-10-01 20:35:08 +0530

    Update loop.js

--- loop.js
+++ loop.js
@@ -5,7 +5,7 @@ function guess()
     let us= prompt("Guess a number from 0-100");
     while(us != guessNo)
         {
-            us= prompt("WRONG! Take Another Guess"); // this gives a string != to number type
+            us= prompt("WRONG! Take Another Guess"); 
         }
     alert("CORRECT! Guess");
 }
